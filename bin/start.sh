08195160ab5404391ce7e9cc30152ca371671cf8
starry0819 <997543429@qq.com>
2018-03-26 11:12:49 +0800

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -1,10 +1,10 @@
 # startup.sh 启动项目
 #!/bin/sh
-export JAVA_HOME=/home/shopin/jdk1.8.0_121
+export JAVA_HOME=/opt/jdk
 echo ${JAVA_HOME}
 echo "授予当前用户权限"
-chmod 777 /home/shopin/mall-search/search-1.0-SNAPSHOT.jar
+chmod 777 /opt/apache-tomcat-8.5.28/webapps/jenkins/workspace/
 echo "执行....."
-cd /home/shopin/mall-search/
-nohup ${JAVA_HOME}/bin/java -jar search-1.0-SNAPSHOT.jar > /dev/null &
+cd /opt/apache-tomcat-8.5.28/webapps/jenkins/workspace/SpringBoot-Demo/target/
+nohup ${JAVA_HOME}/bin/java -jar SpringBoot-Demo.jar > /dev/null &
 echo "启动成功..."
